18a9c81ce18d180e3fa36ae802a881435df45153
hellogreg <booksmart@gmail.com>
2015-09-05 16:16:20 -0500

    Experimenting with some basic styles.

--- navlight.js
+++ navlight.js
@@ -1,3 +1,35 @@
-/**
- * Created by Greg on 9/5/15.
- */
+(function () {
+
+  "use strict";
+
+  // Console functions: log() and dir().
+  function log(m) {
+    m = (m !== undefined) ? m : "-----------------"; // If no message, output a separator line.
+    console.log(m);
+  }
+
+  function dir(m) {
+    if (m) {
+      console.dir(m);
+    } else {
+      log("Object returned false.");
+    }
+  }
+
+
+  function navlight() {
+
+    log("navlight() activated!");
+    dir(navlight);
+
+  }
+
+
+  // Quickie DOM readiness check to initialize widget.
+  if (!!(window.addEventListener)) {
+    window.addEventListener("DOMContentLoaded", navlight);
+  } else {
+    window.attachEvent("onload", navlight);
+  }
+
+}());
